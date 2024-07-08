39c7baf207534ff45cdd3357fab8361e73e66baf
MoonSongI <msongi061@gmail.com>
2024-07-08 19:09:28 +0900

    반려동물 등록, 수정, 삭제 구현 (수정 필요)

--- veiwServer/src/main/java/com/example/veiwServer/controller/MyPetController_songi.java
+++ veiwServer/src/main/java/com/example/veiwServer/controller/MyPetController_songi.java
@@ -1,7 +1,9 @@
 package com.example.veiwServer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
@@ -11,5 +13,19 @@ public class MyPetController_songi {
 	public String myPetForm() {
 		
 		return "/user/my_pet_form";
+	}
+	
+	@RequestMapping("/myPetList")
+	public String myPetList() {
+		
+		return "/user/my_pet_list";
+	}
+	
+	
+	@RequestMapping("/myPetEdit")
+	public String myPetEdit(@RequestParam("id")Long id, Model model) {
+		model.addAttribute("id",id);
+		
+		return "/user/my_pet_edit";
 	}
 }
