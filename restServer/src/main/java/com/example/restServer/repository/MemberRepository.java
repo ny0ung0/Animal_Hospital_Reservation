0699753f577708bb16dfc7ad77174662a1e7391b
Jisun Kim <wltjs5963@naver.com>
2024-07-08 17:50:14 +0900

    관리자 병원 가입 승인 기능 및 페이지 구현 완료(이메일 보내기는 추후 예정)

--- restServer/src/main/java/com/example/restServer/repository/MemberRepository.java
+++ restServer/src/main/java/com/example/restServer/repository/MemberRepository.java
@@ -1,10 +1,17 @@
 package com.example.restServer.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.restServer.dto.IMemberLoginDto;
 import com.example.restServer.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member,Long>{
 
 	public Member findByNickname(String nickname);
+
+	@Query(value = "SELECT m.*, l.username, l.id AS login_id FROM member m JOIN login l ON m.id = l.member_id WHERE m.role='ROLE_HOSPITAL' AND m.status IS NULL", nativeQuery = true)
+	public List<IMemberLoginDto> findByStatusNull();
 }
