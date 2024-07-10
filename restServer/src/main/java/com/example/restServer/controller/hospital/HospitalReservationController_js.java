33528bc0dda8b1cbdbb95a5d1b9625b7b6a6919c
Jisun Kim <wltjs5963@naver.com>
2024-07-10 12:19:22 +0900

    예약 취소시 포인트 반환

--- restServer/src/main/java/com/example/restServer/controller/hospital/HospitalReservationController_js.java
+++ restServer/src/main/java/com/example/restServer/controller/hospital/HospitalReservationController_js.java
@@ -1,5 +1,6 @@
 package com.example.restServer.controller.hospital;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,8 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.restServer.entity.Doctor;
+import com.example.restServer.entity.Member;
+import com.example.restServer.entity.Point;
 import com.example.restServer.entity.Reservation;
 import com.example.restServer.repository.DoctorRepository;
+import com.example.restServer.repository.PointRepository;
 import com.example.restServer.repository.ReservationRepository;
 
 @CrossOrigin("*")
@@ -28,6 +32,9 @@ public class HospitalReservationController_js {
 	
 	@Autowired
 	DoctorRepository doctorRepo;
+	
+	@Autowired
+	PointRepository pointRepo;
 	
 	@GetMapping("/reservation/waiting")
 	public ResponseEntity<List<Reservation>> getWaitingReservation(){
@@ -73,11 +80,20 @@ public class HospitalReservationController_js {
 	@PutMapping("/reservation/{id}/{status}")
 	public ResponseEntity<Reservation> updateReservationStatus(@PathVariable("id") Long id, @PathVariable("status")String status){
 		System.out.println("예약 상태 업데이트");
+		
 		Optional<Reservation> result = reservationRepo.findById(id);
 		Reservation reservation = result.get();
 		reservation.setStatus(status);
 		Reservation reservation2 = reservationRepo.save(reservation);
 		System.out.println(reservation2);
+		if(status.equals("취소")) {
+			Point point = new Point();
+			point.setUser(reservation.getUser());
+			point.setPointsAccumulated(reservation.getPointsUsed());
+			point.setComment("예약 취소 포인트 반환");
+			point.setAccumulationDate(new Date());
+			pointRepo.save(point);
+		}
 		return new ResponseEntity<>(reservation2, HttpStatus.OK);
 	}
 	
@@ -86,6 +102,12 @@ public class HospitalReservationController_js {
 		System.out.println("의사 리스트 가져오기");
 		List<Doctor> list =  doctorRepo.findAllByHospitalId(3L);
 		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+	
+	@GetMapping("/business-hours/{date}")
+	public ResponseEntity<String> getHospitalBusinessHours(@PathVariable("date") String date){
+		System.out.println(date);
+		return new ResponseEntity<>("list", HttpStatus.OK);
 	}
 	
 }
