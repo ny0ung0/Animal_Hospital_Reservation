51343bee98a58bd3f04c7f0e1c1146a81f10e3c1
Jisun Kim <wltjs5963@naver.com>
2024-07-10 11:24:24 +0900

    컨트롤러 정리

--- restServer/src/main/java/com/example/restServer/controller/admin/AdminController_jisun.java
+++ restServer/src/main/java/com/example/restServer/controller/admin/AdminController_jisun.java
@@ -1,19 +1,12 @@
 package com.example.restServer.controller.admin;
 
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -24,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.restServer.dto.IMemberLoginDto;
