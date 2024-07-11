5d11afffe6e02d9e3715f2480f57cd3b21e1c6f9
jun <wnsgud0202@naver.com>
2024-07-11 12:05:40 +0900

    영업정보 불러오기 오류 해결

--- restServer/src/main/java/com/example/restServer/dto/MemberEditDto.java
+++ restServer/src/main/java/com/example/restServer/dto/MemberEditDto.java
@@ -1,5 +1,9 @@
 package com.example.restServer.dto;
 
+import java.time.LocalDateTime;
+
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,14 +14,48 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class MemberEditDto {
+	//private String username;
+	//private String password;
+	private String name;
+	//private String address;
+	//private String phone;
+	private String nickname;    
+	//private String email;
+	private Long memberId;
+	
+	//private Long id;
+	//로그인 테이블
 	private String username;
 	private String password;
-	private String name;
-	private String address;
-	private String phone;
-	private String nickname;    
-	private String email;
-	private Long memberId;
+	private String passwordOrigin;
+	private String passwordNew;
+	private String passwordCheckBox;
+	//의사 테이블
+    private String doctorNamesField;
+    //멤버 테이블
+    private String address;
+    private String phone;
+    private String email;
+    private String hospitalName;
+    private String representative;
+    private String businessHours;
+    private Boolean partnership;
+    private String introduction;
+   
+    private MultipartFile file;
+    private String logo;
+   
+    
+    private String token;
+    private String status;
+    private String role;
+    private String businessNumber;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    
+    public String getFileName() {
+		return file.getOriginalFilename();
+	}
     
 }
 
