71713fc738d3ade8ea376219e190be40a317a8fb
Jia <jiayoo.dev@gmail.com>
2024-07-11 15:51:12 +0900

    상세페이지 및 리스트페이지에서 북마크 기능 추가, 위치정보 허용안하면 지도 부분 알림메세지띄워주기, 예약에서 포인트제휴병원아니면 포인트 못넣도록 수정

--- veiwServer/src/main/resources/static/js/vet_list_region.js
+++ veiwServer/src/main/resources/static/js/vet_list_region.js
@@ -109,6 +109,7 @@ searchBtn.addEventListener("click", function() {
 								       		"partnership" : hospital.partnership, 
 								       		"businessHours" : hospital.businessHours};
 	       	})
+	       	console.log(memVet)
         }
         fetchHospitalData(guMap, Array.from(citiesWithNoGu));
     };
@@ -121,7 +122,7 @@ searchBtn.addEventListener("click", function() {
     
     const url = "http://localhost:9001/api/v1/vet-list?" + params.toString();
     xhttp.open("GET", url, true);
-//    xhttp.setRequestHeader("username", 1);
+    xhttp.setRequestHeader("username", 1);
     xhttp.send();
 });
 
@@ -169,84 +170,32 @@ function addHospitalToList(hospital) {
 	let phone = hospital["소재지전화"] ? hospital["소재지전화"] : '';
     let listItem = document.createElement("div");
     listItem.classList = "vet"
-	listItem.innerHTML = '<button type="button" onclick="showModal(event)" class="btn btn-hospital-sub" data-bs-toggle="modal" data-bs-target="#exampleModal">'
-	        + hospital["사업장명"] + '</button> , '
-	        + '<img class="pin" style="width:35px; display:none;" src="/images/pin_p.svg"/>'
-	        + '<img class="bookmark" style="width:35px;" src="/images/bookmark.png"/>'
-	        + '<span class="phone">' + phone
-	        + '</span> , <span class="address">' + hospital["소재지전체주소"] + '</span>';
+	listItem.innerHTML = '<div class="vet-header">' +
+					        '<button type="button" onclick="showModal(event)" class="btn btn-hospital-sub" data-bs-toggle="modal" data-bs-target="#exampleModal">' +
+					            hospital["사업장명"] +
+					        '</button>' +
+					        '<img class="pin" style="width:35px; display:none;" src="/images/pin_p.svg"/>' +
+					        '<img onclick="checkBookmark(event)" class="bookmark" style="display:none; width:35px;" src="/images/bookmark.png"/>' +
+					     '</div>' +
+					     '<div class="vet-body">' +
+					       '<span class="phone">' + phone + '</span> <span class="address">' + hospital["소재지전체주소"] + '</span>' +
+					     '</div>';
 	document.querySelector(".vet_list").appendChild(listItem);
 	
 	if(memVet[hospital["사업장명"]] != null && memVet[hospital["사업장명"]]["address"] == hospital["소재지전체주소"]){
 		listItem.querySelector("button").classList="btn btn-user-sub"
-		listItem.querySelector(".phone").innerText = memVet[hospital["사업장명"]]["phone"]
+		listItem.querySelector("button").setAttribute("data-id", memVet[hospital["사업장명"]]["id"])
+        listItem.querySelector(".phone").innerText = memVet[hospital["사업장명"]]["phone"];
+        listItem.querySelector(".bookmark").style.display="inline-block"
+        listItem.querySelector(".bookmark").src = memVet[hospital["사업장명"]]["bookmarked"] ? "/images/bookmark_fill.png" : "/images/bookmark.png";
+		
 		if(memVet[hospital["사업장명"]]["partnership"] == true){
 			listItem.querySelector("img").style.display="inline-block"
 		}
 	}
 }
 
-//모달에 해당 병원 상세정보 보여주기
-function showModal(e){
-	//회원만 있는 병원 정보칸 가리기
-	document.querySelector(".modal-memVetInfo").style.display = "none";
-	
-	
-	let hospitalName = e.target.parentElement.querySelector("button").innerText;
-	let address = e.target.parentElement.querySelector(".address").innerText;
-	
-	
-	if(memVet[hospitalName] != null && memVet[hospitalName]["address"] == address){
-		document.querySelector("#working_hour").innerHTML = "";
-		let basicHours = JSON.parse(memVet[hospitalName]["businessHours"]);
-		let hoursArr = getBasicBusinessHours(basicHours);
 
-		//회원만 있는 병원 정보칸 보이게하기
-		document.querySelector(".modal-memVetInfo").style.display = "block";
-
-		// 병원 id 설정
-		document.querySelector("#hospital_id").innerHTML = memVet[hospitalName]["id"];
-		// 영업시간 설정
-		document.querySelector("#working_hour").style.display = "block";
-		showBusinessHour(hoursArr);
-		// 리뷰 설정
-		document.querySelector("#review").style.display = "inline-block";
-		document.querySelector("#review").innerHTML = memVet[hospitalName]["review"];
-		// 예약하기 버튼 설정
-		document.querySelector(".reservationBtn").style.display = "block";
-		// 채팅 버튼 설정
-		document.querySelector(".chatBtn").style.display = "block";
-		// 포인트제휴여부 설정
-		document.querySelector("#point").style.display = "inline-block";
-		document.querySelector("#point").innerHTML = memVet[hospitalName]["partnership"] ? "포인트제휴병원 ⭕" : "포인트제휴병원 ❌";
-		// 북마크 설정
-		document.querySelector("#bookmarked").style.display = "inline-block";
-		document.querySelector("#bookmarked").innerHTML = memVet[hospitalName]["bookmarked"];
-		// 사업자번호 설정
-		document.querySelector("#businessNumber").style.display = "inline-block";
-		document.querySelector("#businessNumber").innerHTML = memVet[hospitalName]["businessNumber"];
-		// 이메일 설정
-		document.querySelector("#email").style.display = "inline-block";
-		document.querySelector("#email").innerHTML= memVet[hospitalName]["email"];
-		// 소개글 설정
-		document.querySelector("#introduction").style.display = "block";
-		document.querySelector("#introduction").innerHTML = memVet[hospitalName]["introduction"];
-		if(memVet[hospitalName]["logo"] != null){
-			// 로고 설정
-			document.querySelector("#logo").style.display = "block";
-			document.querySelector("#logo").src = "/images/user/" + memVet[hospitalName]["logo"];
-		}
-		// 대표자 설정
-		document.querySelector("#representative").style.display = "inline-block";
-		document.querySelector("#representative").innerHTML = memVet[hospitalName]["representative"];
-		// 평균별점 설정
-		document.querySelector("#avgReview").style.display = "inline-block";
-		document.querySelector("#avgReview").innerHTML = memVet[hospitalName]["avgReview"];
-	}
-	document.querySelector("#exampleModalLabel").innerText = hospitalName;
-	document.querySelector("#phone").innerHTML = e.target.parentElement.querySelector(".phone").innerText;
-	document.querySelector("#address").innerHTML = address;
-}
 
 function resetFilter(){
 	vetContainer.innerHTML="";
@@ -261,11 +210,11 @@ function resetFilter(){
 	})
 }
 
-function sortingReserv(e){
-	console.log("예약가능병원순")
-	console.log(searchResult)
-	console.log(memVet)
-}
+//function sortingReserv(e){
+//	console.log("예약가능병원순")
+//	console.log(searchResult)
+//	console.log(memVet)
+//}
 
 
 //function sortingPoint(e) {
@@ -296,10 +245,5 @@ function sortingReserv(e){
 //    console.log("searchResult after sort:", searchResult);
 //}
 
-function makeReservation(){
-	if(localStorage.getItem("username") == null || localStorage.getItem("role") != "ROLE_USER"){
-		alert("로그인한 회원만 사용할 수 있는 기능입니다. 로그인 후 이용해주세요")
-	}else{
-		console.log("예약하기 페이지로 가기")
-	}
-}
+
+
