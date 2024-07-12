7aa4a15d8f47916ad9a1275c1ccf68b5b8f84428
MoonSongI <msongi061@gmail.com>
2024-07-12 11:51:07 +0900

    반려동물, 고객센터 관련 기능 ->  요청헤더에 토큰 값 보내고 헤더 member ID 로 유저값이 저장되게 수정

--- restServer/src/main/java/com/example/restServer/controller/user/MyPetController_songi.java
+++ restServer/src/main/java/com/example/restServer/controller/user/MyPetController_songi.java
@@ -1,37 +1,32 @@
 package com.example.restServer.controller.user;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.restServer.dto.PetDto;
+import com.example.restServer.entity.Member;
 import com.example.restServer.entity.Pet;
+import com.example.restServer.repository.MemberRepository;
 import com.example.restServer.repository.PetRepository;
 import com.example.restServer.service.user.PetService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 
 @RestController
@@ -45,14 +40,26 @@ public class MyPetController_songi {
 	@Autowired
 	private PetRepository petRepository;
 	
+	@Autowired
+	private MemberRepository memberRepo;
+	
 	
 	@Value("${spring.servlet.multipart.location}")
     private String uploadPath;
 	
 	
 	@PostMapping("/myPet")
-	public ResponseEntity<?> myPetRegist(PetDto petDto , @RequestParam("photo") MultipartFile file ) {
-		
+	public ResponseEntity<?> myPetRegist(PetDto petDto , @RequestParam("photo") MultipartFile file, HttpServletRequest request ) {
+		   String memberIdHeader = request.getHeader("MemberId");
+		   String authHeader = request.getHeader("Authorization");
+
+		   if (memberIdHeader == null || authHeader == null) {
+	    	   String errorMessage = "MemberId 또는 Authorization 헤더가 없습니다.";
+	    	    return ResponseEntity.badRequest().body(errorMessage);
+	       }
+
+		   Long memberId = Long.parseLong(memberIdHeader);
+		   Member member = memberRepo.findById(memberId).get();
 		
 	    try {
 	    
@@ -65,7 +72,7 @@ public class MyPetController_songi {
 	        pet.setType(petDto.getType());
 	        pet.setBigtype(petDto.getBigtype());
 	        pet.setWeight(petDto.getWeight());
-	        
+	        pet.setMember(member);
 	        
 	        String imgOriginName = petDto.getFileName();
 	        String imgNewName = UUID.randomUUID().toString() + "_" + imgOriginName;
@@ -97,17 +104,41 @@ public class MyPetController_songi {
 
 	
 	@GetMapping("/myPet")
-	public List<Pet> myPetList() {
-		List<Pet> petList = petRepository.findAll();
+	public ResponseEntity<?> myPetList(HttpServletRequest request) {
+		
+	   String memberIdHeader = request.getHeader("MemberId");
+	   String authHeader = request.getHeader("Authorization");
+
+	   if (memberIdHeader == null || authHeader == null) {
+    	   String errorMessage = "MemberId 또는 Authorization 헤더가 없습니다.";
+    	    return ResponseEntity.badRequest().body(errorMessage);
+       }
+
+	   Long memberId = Long.parseLong(memberIdHeader);
+	   Member member = memberRepo.findById(memberId).get();
+
+
+		List<Pet> petList = petRepository.findByMember(member);
 		System.out.println("펫 목록 출력 : " + petList );
-		return petList;
+		
+		return ResponseEntity.ok(petList);
 	}
 	
 	
 	
 	@GetMapping("/myPet/{id}")
-	public ResponseEntity<Pet> myPetDetail(@PathVariable("id") Long id) {
-	    System.out.println("id출력: " + id);
+	public ResponseEntity<?> myPetDetail(@PathVariable("id") Long id, HttpServletRequest request) {
+	   System.out.println("펫정보 불러오기 id값 출력: " + id);
+	    
+	   String memberIdHeader = request.getHeader("MemberId");
+	   String authHeader = request.getHeader("Authorization");
+
+	   if (memberIdHeader == null || authHeader == null) {
+    	   String errorMessage = "MemberId 또는 Authorization 헤더가 없습니다.";
+    	    return ResponseEntity.badRequest().body(errorMessage);
+       }
+
+	    
 	    Optional<Pet> optionalPet = petRepository.findById(id);
 	    if (optionalPet.isPresent()) {
 	        Pet pet = optionalPet.get();
@@ -121,8 +152,16 @@ public class MyPetController_songi {
 	
 	
 	@PutMapping("/myPet/{id}")
-	public ResponseEntity<?> myPetEdit(@PathVariable("id")Long id,PetDto petDto) {
-		
+	public ResponseEntity<?> myPetEdit(@PathVariable("id")Long id,PetDto petDto, HttpServletRequest request) {
+		 String memberIdHeader = request.getHeader("MemberId");
+		   String authHeader = request.getHeader("Authorization");
+
+		   if (memberIdHeader == null || authHeader == null) {
+	    	   String errorMessage = "MemberId 또는 Authorization 헤더가 없습니다.";
+	    	    return ResponseEntity.badRequest().body(errorMessage);
+	       }
+	       
+	       
 		try {
 			Pet pet = petRepository.findById(id).get();
 			
@@ -147,7 +186,20 @@ public class MyPetController_songi {
 	
 	
 	@DeleteMapping("/myPet/{id}")
-	public ResponseEntity<String> myPetDelte(@PathVariable("id")Long id) {
+	public ResponseEntity<String> myPetDelte(@PathVariable("id")Long id, HttpServletRequest request) {
+	   String memberIdHeader = request.getHeader("MemberId");
+	   String authHeader = request.getHeader("Authorization");
+
+	   if (memberIdHeader == null || authHeader == null) {
+    	   String errorMessage = "MemberId 또는 Authorization 헤더가 없습니다.";
+    	    return ResponseEntity.badRequest().body(errorMessage);
+       }
+
+	   //Long memberId = Long.parseLong(memberIdHeader);
+	   //Member member = memberRepo.findById(memberId).get();
+		
+		
+		
 		petRepository.deleteById(id);
 		
 		return ResponseEntity.ok("삭제 완료"); 
