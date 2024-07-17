c6b1d5662b216685634c589d84fdee6dd4e4d7fc
ny0ung0 <dlagusdyd347398@daum.net>
2024-07-17 16:46:21 +0900

    대상 지정 알람 발송 기능 추가 / 일반 이용자가 병원 예약 시 병원에 예약이 들어왔다는 알람 발송. / 알람 보내는 js 구분.

--- restServer/src/main/java/com/example/restServer/controller/FcmController.java
+++ restServer/src/main/java/com/example/restServer/controller/FcmController.java
@@ -1,12 +1,12 @@
 package com.example.restServer.controller;
 
 import java.io.IOException;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -15,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.restServer.dto.FcmSendDto;
 import com.example.restServer.entity.Member;
+import com.example.restServer.entity.Notification;
 import com.example.restServer.repository.MemberRepository;
+import com.example.restServer.repository.NotificationRepository;
 import com.example.restServer.service.FcmService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -28,6 +30,9 @@ public class FcmController {
 	@Autowired
 	MemberRepository memberRepository;
 
+	@Autowired
+	NotificationRepository notificationRepository;
+
 	private final FcmService fcmService;
 
 	public FcmController(FcmService fcmService) {
@@ -35,16 +40,53 @@ public class FcmController {
 	}
 
 	@PostMapping("/send")
-	public ResponseEntity<Object> pushMessage(@RequestBody @Validated FcmSendDto fcmSendDto,
+	public ResponseEntity<Object> saveToken(@RequestBody @Validated FcmSendDto fcmSendDto,
 			@RequestHeader(value = "MemberId", required = false) Long memberId) throws IOException {
 		System.out.println(fcmSendDto.getToken());
 		System.out.println(fcmSendDto.toString());
-		log.debug("[+] 푸시 메시지를 전송합니다. ");
+		log.debug("[+] fcm 토큰 저장 ");
 		Member member = memberRepository.findById(memberId).get();
 		member.setToken(fcmSendDto.getToken());
 		memberRepository.save(member);
 
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@PostMapping("/message")
+	public ResponseEntity<Object> pushMessagetoMe(@RequestBody @Validated FcmSendDto fcmSendDto,
+			@RequestHeader(value = "MemberId", required = false) Long memberId) throws IOException {
+		
+		log.debug("[+] 메세지 전송 ");
+		Member receiver = memberRepository.findById(memberId).get();
+		Member sender = memberRepository.findById(13L).get();
+		
+		Notification notification = Notification.builder().sender(sender).receiver(receiver).title(fcmSendDto.getTitle())
+				.content(fcmSendDto.getBody()).isRead(false).build();
+
+		notificationRepository.save(notification);
+
+
 		int result = fcmService.sendMessageTo(fcmSendDto);
+
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+	@PostMapping("/message/{receiverId}")
+	public ResponseEntity<Object> pushMessagetoReceiver(@RequestBody @Validated FcmSendDto fcmSendDto,
+			@RequestHeader(value = "MemberId", required = false) Long memberId,
+			@PathVariable(value = "receiverId") Long receiverId) throws IOException {
+		log.debug("[+] 메세지 전송 ");
+
+		Member sender = memberRepository.findById(memberId).get();
+		Member receiver = memberRepository.findById(receiverId).get();
+		Notification notification = Notification.builder().sender(sender).receiver(receiver).title(fcmSendDto.getTitle())
+				.content(fcmSendDto.getBody()).isRead(false).build();
+
+		notificationRepository.save(notification);
+		System.out.println("receiver" + receiver.getToken());
+		fcmSendDto.setToken(receiver.getToken());
+		int result = fcmService.sendMessageTo(fcmSendDto);
+		System.out.println(fcmSendDto.toString());
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 }
---
+++ veiwServer/src/main/resources/static/js/fcmsend.js
@@ -0,0 +1,48 @@
+
+// FCM 나의 알람
+function sendTokenToMessage(title, body) {
+    fetch('http://localhost:9001/api/v1/fcm/message', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'MemberId': localStorage.getItem('MemberId')
+        },
+        body: JSON.stringify({ token: localStorage.getItem('fcm'), title: title, body:body })
+    }).then(response => {
+        console.log('나의 알람:', response);
+    }).catch(error => {
+        console.error('서버로 토큰 전송 중 오류 발생:', error);
+    });
+}
+
+// FCM 대상자 지정 메세지 전송
+function sendTokenToMessageReceiver(title, body){
+	fetch(`http://localhost:9001/api/v1/fcm/message/${receiverId}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'MemberId': localStorage.getItem('MemberId')
+        },
+        body: JSON.stringify({ title: title, body:body })
+    }).then(response => {
+        console.log('나의 알람:', response);
+    }).catch(error => {
+        console.error('서버로 토큰 전송 중 오류 발생:', error);
+    });
+}
+
+// 포그라운드에서 푸시 메시지 수신
+messaging.onMessage((payload) => {
+    console.log('포그라운드 메시지 수신: ', payload);
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/images/logo_user.png'
+    };
+
+    // 브라우저에서 알림을 표시
+    if (Notification.permission === 'granted') {
+        new Notification(notificationTitle, notificationOptions);
+    }
+});
