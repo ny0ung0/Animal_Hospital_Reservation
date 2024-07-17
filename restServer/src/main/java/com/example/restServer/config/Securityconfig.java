3a1abc280ff790dd4ffcf6ac31cf917c0bd0b147
jun <wnsgud0202@naver.com>
2024-07-17 11:56:17 +0900

    시큐리티에서 파이어베이스 permitAll 추가,병원 로고 수정시 기존 로고 삭제,내가쓴 리뷰리스트 완성,회원가입 버튼 작동 수정

--- restServer/src/main/java/com/example/restServer/config/Securityconfig.java
+++ restServer/src/main/java/com/example/restServer/config/Securityconfig.java
@@ -80,12 +80,12 @@ public class Securityconfig {
 		
 		http
 			.authorizeHttpRequests(auth -> auth
-					.requestMatchers("/", "/login", "/images/**","api/v1/common/**",  "/api/v1/near-vet-list", "/api/v1/vet-list").permitAll()
+					.requestMatchers("/", "/login", "/images/**","api/v1/common/**",  "/api/v1/near-vet-list", "/api/v1/vet-list","/api/v1/fcm/**").permitAll()
 					//.requestMatchers("/v3/**","/swagger-ui/**").permitAll()
-					//.requestMatchers("/hospital/**").hasRole("HOSPITAL")
-					//.requestMatchers("/api/v1/user/**").hasAnyRole("USER")
-					//.requestMatchers("/aaa").hasRole("USER")
-					.anyRequest().permitAll() );
+					.requestMatchers("/api/v1/hospital/**").hasRole("HOSPITAL")
+					.requestMatchers("api/v1/user/**").hasRole("USER")
+					.requestMatchers("/api/v1/manager/**").hasRole("ADMIN")
+					.anyRequest().authenticated() );
 			
 		//세션 설정 : Stateless
 		http
