338906325a5f0caf1cac0c6279cbecce0dfeeec7
ny0ung0 <dlagusdyd347398@daum.net>
2024-07-23 16:00:41 +0900

    병원 측 헤더에 알람 추가, x버튼 클릭 시 목록에서 사라지도록 업데이트, 알람 온 갯수 체크 가능하도록 업데이트, 알람 개수 0개면 뱃지 보이지 않도록 업데이트, controller url 변경 및 업데이트 알람 iframe을 통해서 화면에 나오도록 기능

--- restServer/src/main/java/com/example/restServer/repository/NotificationRepository.java
+++ restServer/src/main/java/com/example/restServer/repository/NotificationRepository.java
@@ -1,8 +1,18 @@
 package com.example.restServer.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.restServer.entity.Notification;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+	List<Notification> findByReceiverIdAndIsReadFalse(Long receiverId);
+	
+	@Query("SELECT COUNT(n) FROM Notification n WHERE n.receiver.id = :memberId AND n.isRead = false")
+    Long countByReceiver(@Param("memberId") Long memberId);
+	
 }
