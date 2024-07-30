9c825d51601acf8cafa56956840be02114d0af3a
ny0ung0 <dlagusdyd347398@daum.net>
2024-07-30 15:27:30 +0900

    포인트 적립 및 사용 내역 리스트 출력 추가.

--- restServer/src/main/java/com/example/restServer/controller/user/PointController.java
+++ restServer/src/main/java/com/example/restServer/controller/user/PointController.java
@@ -1,11 +1,14 @@
 package com.example.restServer.controller.user;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.restServer.entity.Point;
 import com.example.restServer.repository.PointRepository;
 
 @RestController
@@ -20,4 +23,11 @@ public class PointController {
 		
 		return pointRepository.findByUserIdRemainingPoints(memberId);
 	}
+	
+	@GetMapping("/pointList/{memberId}")
+	public List<Point> getpointList(@PathVariable(value = "memberId") Long memberId){
+		
+		return pointRepository.findAllByUserId(memberId);
+	}
+	
 }
