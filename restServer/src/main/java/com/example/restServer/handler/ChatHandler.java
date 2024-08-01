5db43ffa718526307c4ca3755d90173a264e27cb
Moon SongI <msongi061@gmail.com>
2024-08-01 23:10:27 +0900

    채팅 스크립트 기능 수정

--- restServer/src/main/java/com/example/restServer/handler/ChatHandler.java
+++ restServer/src/main/java/com/example/restServer/handler/ChatHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import com.example.restServer.entity.Chat;
+import com.example.restServer.entity.ChatRoom;
 import com.example.restServer.entity.Member;
 import com.example.restServer.repository.MemberRepository;
 import com.example.restServer.service.ChatService;
@@ -40,13 +41,8 @@ public class ChatHandler extends TextWebSocketHandler{
 	
 	@Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		// 1. WebSocket에서 수신한 메시지를 Chat 객체로 변환합니다.
-	    //Chat chatMessage = objectMapper.readValue(message.getPayload(), Chat.class);
-	    
-	    // WebSocket에서 수신한 메시지를 JsonNode로 변환합니다.
+		
         JsonNode jsonNode = objectMapper.readTree(message.getPayload());
-        
-        
         
         if (jsonNode.has("type") && jsonNode.get("type").asText().equals("join")) {
             Long senderId = jsonNode.get("sender").asLong();
@@ -98,6 +94,27 @@ public class ChatHandler extends TextWebSocketHandler{
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         list.add(session);
         log.info(session + " 클라이언트 접속");
+        
+        
+        Long senderId = (Long) session.getAttributes().get("sender");
+        Long receiverId = (Long) session.getAttributes().get("receiver");
+        
+        Member sender = memberRepo.findById(senderId).get();
+        Member receiver = memberRepo.findById(receiverId).get();
+	    		
+        if (senderId != null && receiverId != null) {
+            // 데이터베이스에 채팅방 생성
+            ChatRoom chatRoom= chatService.getOrCreateChatRoom(sender, receiver);
+            log.info("채팅방 생성됨: sender=" + senderId + ", receiver=" + receiverId);
+            Long chatRoomId = chatRoom.getId();
+            String responseMessage = "ChatRoom ID: " + chatRoomId;
+            session.sendMessage(new TextMessage(responseMessage));
+        } else {
+            log.error("채팅방 생성 실패: sender 또는 receiver 정보가 없음");
+            session.sendMessage(new TextMessage("채팅방 생성 실패: sender 또는 receiver 정보가 없음"));
+        }
+        
+        
     }
 	
     /* Client가 접속 해제 시 호출되는 메서드드 */
